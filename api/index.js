bf660abfba2edf760cc47043f84a54a8d4267341
cristianTabares86 <cristiantabares.iq@gmail.com>
2023-05-05 08:04:57 -0500

    2do commit

--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = process.env.PORT || 3001
+//const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-server.listen(port, () => {
-    console.log("%s listening at " + port); // eslint-disable-line no-console
+server.listen(5432, () => {
+    console.log("%s listening at 5432"); // eslint-disable-line no-console
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  `postgres://postgres:mia081013@localhost:5432/gameshop`,
+  'postgres://dbgameshop:Vl3JBpfDhlKyVZY6GWVC71dA7VeKhcDW@dpg-chafqk67avj5o4b9gs4g-a.oregon-postgres.render.com/gameshop_1oeb?ssl=true',
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
