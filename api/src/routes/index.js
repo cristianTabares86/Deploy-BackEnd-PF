6dc09aa293713ac4dd0f5fe62071886cd064a1ae
cristianTabares86 <cristiantabares.iq@gmail.com>
2023-05-05 11:34:29 -0500

    16vo commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -18,7 +18,7 @@ const createBulkDB = async (req, res) => {
             "https://api.rawg.io/api/games?page_size=10&key=6df927ecdff443ffa74507df2223a6ad&page_size=150"); // ? solicita los datos a la api externa
             //console.log(response.data)
             const allGames = response.data.results.map((game) => ({//? trae los datos unificando el formato
-                //id: game.id,
+                id: game.id,
                 name: game.name,
                 rating: game.rating,
                 platforms: game.platforms.map((platform) => platform.platform.name),        
