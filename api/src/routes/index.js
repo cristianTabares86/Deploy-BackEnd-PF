0c9b22f1c3398a294123885f332bb7058611d29f
cristianTabares86 <cristiantabares.iq@gmail.com>
2023-05-05 11:27:16 -0500

    15vo commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -15,7 +15,7 @@ const createBulkDB = async (req, res) => {
     try {  
         console.log("estoy en createBulkDB")
         const response = await axios.get(
-            "https://api.rawg.io/api/games?page_size=10&key=6df927ecdff443ffa74507df2223a6ad&page_size=5"); // ? solicita los datos a la api externa
+            "https://api.rawg.io/api/games?page_size=10&key=6df927ecdff443ffa74507df2223a6ad&page_size=150"); // ? solicita los datos a la api externa
             //console.log(response.data)
             const allGames = response.data.results.map((game) => ({//? trae los datos unificando el formato
                 //id: game.id,
